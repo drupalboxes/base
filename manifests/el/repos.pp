4422553241de3bb0961084edd2782d4f59481625
Rob Knight <mail@robknight.org.uk>
2013-05-16 22:48:11 +0100

    Fixed yum repository code to handle GPG keys correctly and made GPG keys major version-specific

--- manifests/el/repos.pp
+++ manifests/el/repos.pp
@@ -1,22 +1,37 @@
 class base::el::repos {
+  $version_array = split($operatingsystemrelease, '\.')
+  $major_version = $version_array[0]
+
+  file { "epel-gpgkey":
+    path    => '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL',
+    source  => "puppet:///modules/base/el/${major_version}/RPM-GPG-KEY-EPEL",
+    before  => Yumrepo['epel']
+  }
+
   yumrepo { 'epel':
     enabled         => 1,
     failovermethod  => 'priority',
     gpgcheck        => 1,
-    gpgkey          => 'puppet:///modules/base/el/RPM-GPG-KEY-EPEL',
-    descr           => 'Extra Packages for Enterprise Linux 6 - $basearch',
-    baseurl         => 'http://download.fedoraproject.org/pub/epel/6/$basearch',
+    gpgkey          => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL",
+    descr           => "Extra Packages for Enterprise Linux ${major_version} - \$basearch",
+    baseurl         => "http://download.fedoraproject.org/pub/epel/${major_version}/\$basearch",
     priority        => 10,
     name            => 'epel'
+  }
+
+  file { "ius-gpgkey":
+    path    => '/etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
+    source  => "puppet:///modules/base/el/${major_version}/IUS-COMMUNITY-GPG-KEY",
+    before  => Yumrepo['ius']
   }
 
   yumrepo { 'ius':
     enabled         => 1,
     descr           => 'IUS Community Project Repo',
     gpgcheck        => 1,
-    baseurl         => 'http://dl.iuscommunity.org/pub/ius/stable/Redhat/6/$basearch/',
+    baseurl         => "http://dl.iuscommunity.org/pub/ius/stable/Redhat/${major_version}/\$basearch/",
     priority        => 1,
-    gpgkey          => 'puppet:///modules/base/el/IUS-COMMUNITY-GPG-KEY',
+    gpgkey          => 'file:///etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY',
     name            => 'ius'
   }
 }
